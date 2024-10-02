35a75d72a268da7d45b627eda44f9b8e6a3a6f2d
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-02 17:28:09 +0200

    added bootstrap, prepared the form with the get call and the action for the new page

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    
+    <section class="container mt-5">
+        <form action="./response.php" method="GET" >
+            <div class="mb-3">
+                <label for="userParagraph" class="form-label">Paragraph</label>
+                <input type="text" class="form-control" id="userParagraph" placeholder="Insert paragraph">
+            </div>
+            <div class="mb-3">
+                <label for="userBadWords" class="form-label">Bad word</label>
+                <input type="text" class="form-control" id="userBadWords" placeholder="Insert the bad word"></input>
+            </div>
+        </form>
+    </section>
 </body>
 </html>
