3a47ddafb5a71f67d064694544dc94a48da3a8dc
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-02 17:35:48 +0200

    prepare the logic

--- response.php
+++ response.php
@@ -8,6 +8,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <?php
+    // prendo i dati dall'url
+    // per il paragrafo
+        $userParagraph = $_GET["userParagraph"];
+    // per la parola da censurare
+        $userBadWords = $_GET["userBadWords"];
+    // sostituisco la parola da censurare con ***
+        $censoredParagraph = str_replace($userBadWords, "***", $userParagraph);
+    ?>
     
 </body>
 </html>
